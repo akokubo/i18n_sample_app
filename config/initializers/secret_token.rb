baf321635d3b7a61f48370a87d23f0c700a98b01
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-06-28 17:10:14 +0900

    アプリケーション初期化

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+I18nSampleApp::Application.config.secret_key_base = '2cd24d43d5d95dd7445adaacda15c8e6188113e9c8b8d9fa2db0cce7de517a8fbaa237036a3ed40a3b00ec9ba19971290e8dac516406267bf43096d97c642845'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+I18nSampleApp::Application.config.session_store :cookie_store, key: '_i18n_sample_app_session'
