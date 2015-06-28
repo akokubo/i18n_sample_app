5c5b219ddd66de8338793a32bfc775764d91a4a1
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-06-28 18:00:19 +0900

    Userオブジェクトを削除すると関連したMicropostオブジェクトを削除する

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,4 +10,23 @@ describe User do
   it { should respond_to(:microposts) }
 
   it { should be_valid }
+
+  describe "micropost associations" do
+    before do
+      @user.save
+      @user.microposts.create([
+        {content: "Lorem ipsum"},
+        {content: "Lorem ipsum"}
+      ])
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+    end
+  end
 end
